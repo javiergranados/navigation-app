d7debf9492b87bd02f2a91076fee2795efd59249
Javier Granados <hola@javiergranados.dev>
2021-12-20 18:17:19 +0100

    Add react-navigation library

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView, StatusBar } from 'react-native';
 import NavigationScreen from './src/screens/NavigationScreen';
 import { appStyles } from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <SafeAreaView style={appStyles.background}>
-      <StatusBar backgroundColor="black" barStyle="light-content" />
-      <NavigationScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={appStyles.background}>
+        <StatusBar backgroundColor="black" barStyle="light-content" />
+        <NavigationScreen />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
--- src/screens/NavigationScreen.tsx
+++ src/screens/NavigationScreen.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 const NavigationScreen = () => {
   return (
