8c7b8f07700ba61f1aad494c34fe201c9285b884
Javier Granados <hola@javiergranados.dev>
2021-12-21 17:30:45 +0100

    Render the drawer navigator as permanent when the device's width is 768px or higher

--- src/navigators/DrawerNavigator.tsx
+++ src/navigators/DrawerNavigator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import StackNavigator from './StackNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
@@ -11,8 +12,15 @@ export type DrawerParamList = {
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 const DrawerNavigator = () => {
+  const { width } = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        ...(width >= 768 && { headerLeft: () => null }),
+        drawerType: width >= 768 ? 'permanent' : 'front',
+      }}
+    >
       <Drawer.Screen name="StackNavigator" options={{ title: 'Home' }} component={StackNavigator} />
       <Drawer.Screen name="SettingsScreen" options={{ title: 'Settings' }} component={SettingsScreen} />
     </Drawer.Navigator>
