4677ae464812ae79bb2031df6a51b9b47a441389
Javier Granados <hola@javiergranados.dev>
2021-12-22 00:30:56 +0100

    Add styles to the material top tab navigator

--- src/navigators/TopTabsNavigator.tsx
+++ src/navigators/TopTabsNavigator.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ChatScreen from '../screens/ChatScreen';
 import ContactsScreen from '../screens/ContactsScreen';
 import AlbumsScreen from '../screens/AlbumsScreen';
+import { colors } from '../theme/appTheme';
 
 export type TabsParamList = {
   ChatScreen: undefined;
@@ -13,8 +15,24 @@ export type TabsParamList = {
 const Tab = createMaterialTopTabNavigator<TabsParamList>();
 
 const TopTabNavigator = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{ paddingTop: insets.top }}
+      sceneContainerStyle={{ backgroundColor: 'white' }}
+      screenOptions={{
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: false,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+      }}
+    >
       <Tab.Screen name="ChatScreen" options={{ title: 'Chat' }} component={ChatScreen} />
       <Tab.Screen name="ContactsScreen" options={{ title: 'Contacts' }} component={ContactsScreen} />
       <Tab.Screen name="AlbumsScreen" options={{ title: 'Albums' }} component={AlbumsScreen} />
