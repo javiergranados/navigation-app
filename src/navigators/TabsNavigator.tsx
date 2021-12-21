2fdb9a555d32406bc2ec21605cd1c7b65ffcf9e6
Javier Granados <hola@javiergranados.dev>
2021-12-21 23:56:54 +0100

    Implement the material bottom tab navigator

--- src/navigators/TabsNavigator.tsx
+++ src/navigators/TabsNavigator.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { colors } from '../theme/appTheme';
 import Tab1Screen from '../screens/Tab1Screen';
 import Tab2Screen from '../screens/Tab2Screen';
 import StackNavigator from './StackNavigator';
+import { Platform } from 'react-native';
 
 export type TabsParamList = {
   Tab1Screen: undefined;
@@ -11,11 +13,10 @@ export type TabsParamList = {
   StackNavigator: undefined;
 };
 
-const Tab = createBottomTabNavigator<TabsParamList>();
-
-const TabsNavigator = () => {
+const TabIOS = createBottomTabNavigator<TabsParamList>();
+const TabsIOSNavigator = () => {
   return (
-    <Tab.Navigator
+    <TabIOS.Navigator
       sceneContainerStyle={{
         backgroundColor: 'white',
       }}
@@ -31,11 +32,35 @@ const TabsNavigator = () => {
         },
       }}
     >
-      <Tab.Screen name="Tab1Screen" options={{ title: 'Tab1', headerShown: false }} component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" options={{ title: 'Tab2', headerShown: false }} component={Tab2Screen} />
-      <Tab.Screen name="StackNavigator" options={{ title: 'Stack', headerShown: false }} component={StackNavigator} />
-    </Tab.Navigator>
+      <TabIOS.Screen name="Tab1Screen" options={{ title: 'Tab1', headerShown: false }} component={Tab1Screen} />
+      <TabIOS.Screen name="Tab2Screen" options={{ title: 'Tab2', headerShown: false }} component={Tab2Screen} />
+      <TabIOS.Screen
+        name="StackNavigator"
+        options={{ title: 'Stack', headerShown: false }}
+        component={StackNavigator}
+      />
+    </TabIOS.Navigator>
   );
+};
+
+const TabMaterial = createMaterialBottomTabNavigator();
+const TabsMaterialNavigator = () => {
+  return (
+    <TabMaterial.Navigator
+      sceneAnimationEnabled
+      barStyle={{
+        backgroundColor: colors.primary,
+      }}
+    >
+      <TabMaterial.Screen name="Tab1Screen" options={{ title: 'Tab1' }} component={Tab1Screen} />
+      <TabMaterial.Screen name="Tab2Screen" options={{ title: 'Tab2' }} component={Tab2Screen} />
+      <TabMaterial.Screen name="StackNavigator" options={{ title: 'Stack' }} component={StackNavigator} />
+    </TabMaterial.Navigator>
+  );
+};
+
+const TabsNavigator = () => {
+  return Platform.OS === 'ios' ? <TabsIOSNavigator /> : <TabsMaterialNavigator />;
 };
 
 export default TabsNavigator;
